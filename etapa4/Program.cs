e6ea76cb97778022f93b552e573563c4a0321de7
Diego A. Dias <dtdias@icatuseguros.com.br>
2021-10-28 10:19:41 -0300

    .

--- etapa4/Program.cs
+++ etapa4/Program.cs
@@ -4,24 +4,113 @@ using System.Collections.Generic;
 
 namespace etapa4
 {
+    public abstract class Veiculo
+    {
+        private bool _automatico;
+        private string _combustivel;
+
+        public void ColocarCombustível()
+        {
+            Console.WriteLine("Basta colocar combustivel necessário");
+        }
+
+        #region Propriedades Veiculo        
+        
+        public bool Automatico
+        {
+            get
+            {
+                return _automatico;
+            }
+            set
+            {
+                _automatico = value;
+            }
+        }
+
+        public string Combustivel
+        {
+            get
+            {
+                return _combustivel;
+            }
+            set
+            {
+                _combustivel = value;
+            }
+        }
+        #endregion
+
+        public Veiculo()
+        {
+            Console.WriteLine("Eu sou um veículo");
+        }
+
+        public abstract void Acelerar();
+        public abstract void Freiar();
+    }
+
+    public class Aviao : Veiculo
+    {
+        public Aviao()
+        {
+            Console.WriteLine("Eu sou um avião.");
+        }
+
+        public override void Acelerar()
+        {
+                
+        }
+
+        public override void Freiar()
+        {
+
+        }
+    }
+
+    public class Carro : Veiculo
+    {
+        public Carro()
+        {
+            Console.WriteLine("Eu sou um carro.");
+        }
+
+        public override void Acelerar()
+        {
+
+        }
+
+        public override void Freiar()
+        {
+
+        }
+    }
+
     public class Colaborador
     {
         private int _matricula;
         private string _nome;
         private string _cpf;
+        private bool _ativo;
+        private Veiculo _veiculo;
 
+        //Não é preciso declarar 
         public Colaborador()
         {
 
         }
 
-        public Colaborador(int matricula, string nome, string cpf)
+        public Colaborador(int matricula, string nome, string cpf, bool ativo, Veiculo veiculo)
         {
             _matricula = matricula;
             _nome = nome;
             _cpf = cpf;
+            _ativo = ativo;
+            _veiculo = veiculo;
         }
 
+        //Usando propriedades, Getter e Setter
+        #region Propriedades Colaborador
         public int Matricula
         {
             get
@@ -58,6 +147,31 @@ namespace etapa4
             }
         }
 
+        public bool Ativo
+        {
+            get
+            {
+                return _ativo;
+            }
+            set
+            {
+                _ativo = value;
+            }
+        }
+
+        public Veiculo Veiculo
+        {
+            get
+            {
+                return _veiculo;
+            }
+            set
+            {
+                _veiculo = value;
+            }
+        }
+        #endregion
+
         public decimal Comissao(decimal salarioColaborador, int percentagem)
         {
             return salarioColaborador * percentagem;
@@ -70,13 +184,36 @@ namespace etapa4
         {
             try
             {
+                Aviao a = new Aviao();               
+                
+                
+                var listaColaboradores = new ArrayList();
+                Colaborador c1 = new Colaborador(111, "aaaa", "cpf1", false, new Carro());
+                Colaborador c2 = new Colaborador(222, "bbb", "cpf2", false, new Carro());
+                Colaborador c3 = new Colaborador(333, "ccc", "cpf3", false, new Aviao());
+
+                listaColaboradores.Add(c1);
+                listaColaboradores.Add(c2);
+                listaColaboradores.Add(c3);
+
+                IList<Colaborador> lC = new List<Colaborador>();
+
+                foreach (Colaborador c in listaColaboradores)
+                {
+                    lC.Add(c);
+                    Console.WriteLine(c.Nome);
+                    Console.WriteLine(c.CPF);
+                    Console.WriteLine(c.Matricula);
+                    Console.WriteLine(c.Veiculo);
+                }
+
+                //Exemplo de Varrendo o nome;
                 var lstNome = new List<string>();
                 string nome = string.Empty;
 
                 lstNome.Add("Diego A. Dias");
                 lstNome.Add("Mateus Oliveira");
                 lstNome.Add("Leonardo da Silva Xavier");
-
 
                 foreach (var var in lstNome)
                 {
@@ -86,11 +223,10 @@ namespace etapa4
                     }
                 }
 
+                //Exemplo calculando dias faltantes;
                 DateTime nascimentoMateus = new DateTime(2001, 01, 22);
-
                 DateTime niverMateus = DateTime.Now; //new DateTime(DateTime.Now.Year, 01, 22);
                 DateTime proxNiverMateus = new DateTime(DateTime.Now.Year + 1, 01, 22);
-
                 TimeSpan t = proxNiverMateus.Subtract(niverMateus);
 
                 Console.WriteLine("Falta " + t.Days.ToString() + " dias para o aniversário do Mateus");
@@ -113,15 +249,13 @@ namespace etapa4
                         break;
                 }
 
-
-
                 int sum1 = 10;
                 int sum2 = 10;
-
                 int vSoma = sum1 + sum2;
                 int vSoma2 = vSoma * 1;
 
-                // && - AND
+                //Operadores
+                //&& - AND
                 // || - OR
                 //  ! - Negação
 
@@ -132,8 +266,7 @@ namespace etapa4
                 else
                     Console.WriteLine("não pagar comissao");
 
-
-                //V         //V
+                          //V         //V
                 if (vSoma >= 20 || vSoma2 == 40)
                 {
                     Console.WriteLine(">= que 20");
@@ -146,7 +279,6 @@ namespace etapa4
                 }
 
                 // % mod - Pega resto de divisão
-
                 int j;
 
                 for (int i = 1; i <= 10; i++)
@@ -154,42 +286,11 @@ namespace etapa4
                     j = sum1 % i;
 
                     Console.WriteLine("j ->" + j);
-
                     Console.WriteLine("**********************");
-
-
-
-                    //Console.WriteLine("multiplicando Sum1 * i (" + i + ")" + sum1 * i);
+                    Console.WriteLine("multiplicando Sum1 * i (" + i + ")" + sum1 * i);
                 }
 
-                Console.ReadLine();
-
-                var listaColaboradores = new ArrayList();
-
-                Colaborador c1 = new Colaborador(111, "aaaa", "cpf1");
-                Colaborador c2 = new Colaborador(222, "bbb", "cpf2");
-                Colaborador c3 = new Colaborador(333, "ccc", "cpf3");
-
-                listaColaboradores.Add(c1);
-                listaColaboradores.Add(c2);
-                listaColaboradores.Add(c3);
-
-                IList<Colaborador> lC = new List<Colaborador>();
-
-                foreach (Colaborador c in listaColaboradores)
-                {
-                    lC.Add(c);
-
-                    Console.WriteLine(c.Nome);
-                    Console.WriteLine(c.CPF);
-                    Console.WriteLine(c.Matricula);
-                }
-
-                ///Continuação ETAPA 4 
-                ///
-
-                // Array com valor nulo
-                int[] primeiroArray;
+                ///Continuação ETAPA 4                 
 
                 // Array instanciado com tamanho 3
                 int[] segundoArray = new int[3];
@@ -201,8 +302,6 @@ namespace etapa4
                 string[] quartoArray = new string[] { "a", "b", "c", "d" };
 
                 Console.WriteLine(quartoArray[0] + quartoArray[1] + quartoArray[2] + quartoArray[3]);
-
-
 
                 List<int> meuList = new List<int>();
 
